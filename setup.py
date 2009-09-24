816307964bc6cabb6703760f2d4784fb0fb0d0d5
borho <devnull@localhost>
2009-09-24 14:36:02 +0000

    because import is a reserved word, zeit.import is renamed to zeit.importer

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zeit.import',
+    version='0.1dev',
+    author='Martin Borho',
+    author_email='martin@borho.net',
+    url='http://trac.gocept.com/zeit',
+    description="""\
+""",
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data = True,
+    zip_safe=False,
+    license='gocept proprietary',
+    namespace_packages = ['zeit'],
+    install_requires=[
+        'zeit.connector',
+        'setuptools',
+        'pytz',
+        ],
+    entry_points = """
+        [console_scripts]  
+        """
+)
---
+++ src/zeit/import/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                format='%(asctime)s %(levelname)-8s %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S')
+
+class AboveInfoFilter(logging.Filter):
+        def filter(self, record):
+            return record.levelno > 20
+
+def add_file_logging (logger, logfile):
+    logfileHndl = logging.FileHandler(logfile)
+    formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+    logfileHndl.setFormatter(formatter)  
+    logfileHndl.addFilter(AboveInfoFilter())
+    logger.addHandler(logfileHndl)
+
