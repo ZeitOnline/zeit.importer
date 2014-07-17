f5bad55bcb8f77e952e12b265412fd0c897b3021
Wolfgang Schnerring <ws@gocept.com>
2014-07-17 16:13:06 +0200

    Preparing release 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.importer',
-    version='0.3.dev0',
+    version='0.2.1',
     author='Martin Borho',
     author_email='martin@borho.net',
     url='http://trac.gocept.com/zeit',
