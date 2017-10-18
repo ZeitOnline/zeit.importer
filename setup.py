190ee6f466de04130ba80b31a5f7e717df246c6d
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-10-18 09:02:05 +0200

    Preparing release 1.4.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.importer',
-    version='1.4.10.dev0',
+    version='1.4.10',
     author='Martin Borho, gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
